293e37e31f3c5e30ac913f833b5ecb3f844d8429
krkbheet@gmail.com <krkbheet@gmail.com>
2024-12-09 13:07:42 +0100

    add dockercompose

--- test-api/script.py
+++ test-api/script.py
@@ -10,20 +10,19 @@ client = MongoClient(MONGO_URI)
 db = client["foot-sensor"]  # Replace with your database name
 collection = db["sensor"]  # Replace with your collection name
 
-@app.route('/test', methods=['POST'])
-def test_api():
-    data = request.json  # Parse the incoming JSON data
+@app.route('/api/data', methods=['POST'])
+def save_data():
+    data = request.json
     if not data:
         return jsonify({"error": "Invalid or missing JSON data"}), 400
 
-    try:
-        # Insert data into MongoDB
-        result = collection.insert_one(data)
-        print(f"Inserted document ID: {result.inserted_id}")
-        return jsonify({"message": "Data received and stored successfully"}), 200
-    except Exception as e:
-        print(f"Error inserting data: {e}")
-        return jsonify({"error": "Failed to store data"}), 500
+    collection.insert_one(data)
+    return jsonify({"message": "Data saved successfully"}), 200
+    
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    documents = list(collection.find({}, {"_id": 0}))
+    return jsonify(documents), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
