1308627f096239de317f28a382e695cea0dfa1b0
krkbheet@gmail.com <krkbheet@gmail.com>
2024-12-27 18:09:01 +0100

    add dataGroup in service

--- frontend-dash/src/app/sensor-data.service.ts
+++ frontend-dash/src/app/sensor-data.service.ts
@@ -6,12 +6,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class SensorDataService {
-  private apiUrl = 'http://127.0.0.1:8080/api/data';  // Update this if necessary
+  private apiUrl = 'http://127.0.0.1:8080/api/data';  // Original endpoint for all data
+  private countApiUrl = 'http://127.0.0.1:8080/api/data/count';  // New endpoint for data count
 
   constructor(private http: HttpClient) {}
 
-  // This method fetches the sensor data from the backend API
+  // This method fetches the full sensor data from the backend API
   getSensorData(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
+  }
+
+  // This method fetches the sensor data count from the backend API
+  getDataCount(): Observable<any> {
+    return this.http.get<any>(this.countApiUrl);
   }
 }
